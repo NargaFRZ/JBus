6501d80d2513d23bd4e5645121ae35caf0d5dfef
NargaFRZ <fairux87@gmail.com>
2023-10-05 21:26:58 +0700

    TP05 - Migrasi ke IntelliJ IDEA

--- src/FairuzMuhammadJBusRA/JBus.java
+++ src/FairuzMuhammadJBusRA/JBus.java
@@ -15,39 +15,16 @@ public class JBus{
      * Main of the class, includes the testing of the other classes
      */
     public static void main(String[] args){
-        Bus b = createBus();
-        Timestamp schedule1 = Timestamp.valueOf("2023-7-18 15:00:00");
-        Timestamp schedule2 = Timestamp.valueOf("2023-7-20 12:00:00");
-        b.addSchedule(schedule1);
-        b.addSchedule(schedule2);
-        b.schedules.forEach(Schedule::printSchedule);
-        // Invalid date
-        Timestamp t1 = Timestamp.valueOf("2023-7-19 15:00:00");
-        System.out.println("Make booking at July 19, 2023 15:00:00 Seat RA12");
-        System.out.println(Payment.makeBooking(t1, "RA12", b));
-        // Valid date, invalid seat
-        Timestamp t2 = Timestamp.valueOf("2023-7-18 15:00:00");
-        System.out.println("\nMake booking at July 18, 2023 15:00:00 Seat RA20");
-        System.out.println(Payment.makeBooking(t2, "RA20", b));
-        // Valid date, valid seat
-        System.out.println("\nMake booking at July 18, 2023 15:00:00 Seat RA07");
-        System.out.println(Payment.makeBooking(t2, "RA07", b));
-        Timestamp t3 = Timestamp.valueOf("2023-7-20 12:00:00");
-        System.out.println("\nMake booking at July 20, 2023 12:00:00 Seat RA01");
-        System.out.println(Payment.makeBooking(t3, "RA01", b));
-        System.out.println("\nMake booking at July 20, 2023 12:00:00 Seat RA01 again");
-        System.out.println(Payment.makeBooking(t3, "RA01", b));
-        // Check if the data changed
-        System.out.println("\nUpdated Schedule\n");
-        b.schedules.forEach(Schedule::printSchedule);
+        System.out.println("Hello from Intellij!");
     }
 
-
+/*
     public static Bus createBus() {
         Price price = new Price(750000, 5);
         Bus bus = new Bus(1, "Netlab Bus", Facility.LUNCH, price, 25, BusType.REGULER, City.BANDUNG, new Station(1, "Depok Terminal", City.DEPOK, "Jl. Margonda Raya"), new Station(2, "Halte UI", City.JAKARTA, "Universitas Indonesia"));
         return bus;
     }
+ */
 
 /*
     public static int getBusID(){
@@ -112,4 +89,33 @@ public class JBus{
         return price * numberOfSeat;
     }
     */
+
+    //pt4 main
+    /*
+    Bus b = createBus();
+        Timestamp schedule1 = Timestamp.valueOf("2023-7-18 15:00:00");
+        Timestamp schedule2 = Timestamp.valueOf("2023-7-20 12:00:00");
+        b.addSchedule(schedule1);
+        b.addSchedule(schedule2);
+        b.schedules.forEach(Schedule::printSchedule);
+        // Invalid date
+        Timestamp t1 = Timestamp.valueOf("2023-7-19 15:00:00");
+        System.out.println("Make booking at July 19, 2023 15:00:00 Seat RA12");
+        System.out.println(Payment.makeBooking(t1, "RA12", b));
+        // Valid date, invalid seat
+        Timestamp t2 = Timestamp.valueOf("2023-7-18 15:00:00");
+        System.out.println("\nMake booking at July 18, 2023 15:00:00 Seat RA20");
+        System.out.println(Payment.makeBooking(t2, "RA20", b));
+        // Valid date, valid seat
+        System.out.println("\nMake booking at July 18, 2023 15:00:00 Seat RA07");
+        System.out.println(Payment.makeBooking(t2, "RA07", b));
+        Timestamp t3 = Timestamp.valueOf("2023-7-20 12:00:00");
+        System.out.println("\nMake booking at July 20, 2023 12:00:00 Seat RA01");
+        System.out.println(Payment.makeBooking(t3, "RA01", b));
+        System.out.println("\nMake booking at July 20, 2023 12:00:00 Seat RA01 again");
+        System.out.println(Payment.makeBooking(t3, "RA01", b));
+        // Check if the data changed
+        System.out.println("\nUpdated Schedule\n");
+        b.schedules.forEach(Schedule::printSchedule);
+     */
 }
